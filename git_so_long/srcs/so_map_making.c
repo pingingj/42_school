bc21246c4e8545aa2da7361c128d4a7a834b572c
root <root@DESKTOP-O8B3TVI.localdomain>
2025-02-18 01:33:09 +0000

    moves omega progress holy shit my brain complicated this shit too much

--- git_so_long/srcs/so_map_making.c
+++ git_so_long/srcs/so_map_making.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_map_making.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgarcez- <dgarcez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:54:47 by dgarcez-          #+#    #+#             */
-/*   Updated: 2025/02/17 17:51:25 by dgarcez-         ###   ########.fr       */
+/*   Updated: 2025/02/18 01:31:42 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,69 +60,44 @@ void	fill_bottom_map(t_mlx *mlx)
 		height++;
 	}
 }
-
-// void	change_number_bg(t_mlx *mlx, int moves, t_img *img, int size)
-// {
-// 	int	x;
-// 	int	y;
-// 	int	position;
-
-// 	position =  mlx.pos.x * 96 / 2
-// 	if(size == 1)
-// 		position += 34
-// 	if(size == 2)
-// 		position -= 18
-// 	if(size == 3)
-// 		position -= 71
-// 	while(x < 38)
-// 	{
-// 		while(y < 85)
-// 		{
-// 			color = pixel_get(img, width, height);
-// 			my_mlx_pixel_put(&mlx->bg_img, x + width, y + height, color);
-// 			y++;
-// 		}
-// 		x++;
-// 	}
-// }
-
-/* void	ft_putnbr(int moves, char **filenames, int *size)
+void	change_number_bg(t_mlx *mlx, int moves, t_img *img, int size)
 {
-	int	result;
+    int	x = 0;
+    int	y = 0;
+	int	position;
 
-	if (moves >= 10)
-		ft_putnbr(moves / 10, filenames, size);
-
-	result = moves % 10;
-	(*size)++;
-	printf("%d and the file name is..... %s and its the %d number\n", result, filenames[result], (*size));
-	// change_number_bg(moves, mlx.numbers.images[result], size);
+	position =  mlx.pos.x * 96 / 2
+	if(size == 1)
+		position += 34
+	if(size == 2)
+		position -= 18
+	if(size == 3)
+		position -= 71
+	while(x < 38)
+	{
+		while(y < 85)
+		{
+			color = pixel_get(img, width, height);
+			my_mlx_pixel_put(&mlx->bg_img, x + width, y + height, color);
+			y++;
+		}
+		x++;
+	}
 }
 
-int CHANGETHIS(int argc, char **argv)
+
+void ft_putnbr(int moves, char **filenames, int position)
 {
-    char    *filenames[10];
-    int     moves;
-	int		size;
-    filenames[0] = "zero.xpm";
-    filenames[1] = "one.xpm";
-    filenames[2] = "two.xpm";
-    filenames[3] = "three.xpm";
-    filenames[4] = "four.xpm";
-    filenames[5] = "five.xpm";
-    filenames[6] = "six.xpm";
-    filenames[7] = "seven.xpm";
-    filenames[8] = "eight.xpm";
-    filenames[9] = "nine.xpm";
-    filenames[10] = NULL;
-	if (argc != 2)
-		return (1);
-	size = 0;
-    moves = atoi(argv[1]);
-	ft_putnbr(moves, filenames, &size);
-	printf("\nsize = %d\n", size);
+    int result;
+
+    result = moves % 10;
+    if (moves >= 10)
+        ft_putnbr(moves / 10, filenames, position + 1); 
+
+     printf("%d and the file name is..... %s and its the %d number\n", 
+    result, filenames[result], position);
+    change_number_bg(t_mlx *mlx, int moves, t_img *img, int size)
 }
- */
 
 void	put_map(t_mlx *mlx, char **map)
 {
